d9ea5ee6c0a093e6c307a96374b71e3020841235
NVCT <38034346+nvctoken@users.noreply.github.com>
2018-07-26 04:55:19 +0530

    We have updated  referral and Airdrop code in the Contract

--- NVCTToken.sol
+++ NVCTToken.sol
@@ -1,196 +1,308 @@
-pragma solidity ^0.4.17;
+pragma solidity ^0.4.24;
 
-// ----------------------------------------------------------------------------
-// Burnable NVCT token contract
-//
-// Symbol           : NVCT
-// Name             : nVision Cash Token
-// Initial Supply   : 850,000,000
-// Decimals         : 18
+/**
+ * @title ERC20Basic
+ * @dev Simpler version of ERC20 interface
+ * @dev see https://github.com/ethereum/EIPs/issues/179
+ */
+contract ERC20Basic {
+  uint256 public totalSupply;
+  function balanceOf(address who) public constant returns (uint256);
+  function transfer(address to, uint256 value) public returns (bool);
+  event Transfer(address indexed from, address indexed to, uint256 value);
+}
 
-// ----------------------------------------------------------------------------
-// Safe math
-// ----------------------------------------------------------------------------
+/**
+ * @title ERC20 interface
+ * @dev see https://github.com/ethereum/EIPs/issues/20
+ */
+contract ERC20 is ERC20Basic {
+  function allowance(address owner, address spender) public constant returns (uint256);
+  function transferFrom(address from, address to, uint256 value) public returns (bool);
+  function approve(address spender, uint256 value) public returns (bool);
+  event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+/**
+ * @title SafeMath
+ * @dev Math operations with safety checks that throw on error
+ */
 library SafeMath {
-    function add(uint a, uint b) internal pure returns (uint c) {
-        c = a + b;
-        require(c >= a);
+  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+    if (a == 0) {
+      return 0;
     }
-    function sub(uint a, uint b) internal pure returns (uint c) {
-        require(b <= a);
-        c = a - b;
-    }
-    function mul(uint a, uint b) internal pure returns (uint c) {
-        c = a * b;
-        require(a == 0 || c / a == b);
-    }
-    function div(uint a, uint b) internal pure returns (uint c) {
-        require(b > 0);
-        c = a / b;
-    }
+    uint256 c = a * b;
+    assert(c / a == b);
+    return c;
+  }
+
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return c;
+  }
+
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
+
+  function add(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a + b;
+    assert(c >= a);
+    return c;
+  }
 }
 
+/**
+ * @title Basic token
+ * @dev Basic version of StandardToken, with no allowances. 
+ */
+contract BasicToken is ERC20Basic {
+    
+  using SafeMath for uint256;
 
-// ----------------------------------------------------------------------------
-// ERC Token Standard #20 Interface
-// https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20-token-standard.md
-// ----------------------------------------------------------------------------
-contract ERC20Interface {
-    function totalSupply() public constant returns (uint);
-    function balanceOf(address tokenOwner) public constant returns (uint balance);
-    function allowance(address tokenOwner, address spender) public constant returns (uint remaining);
-    function transfer(address to, uint tokens) public returns (bool success);
-    function approve(address spender, uint tokens) public returns (bool success);
-    function transferFrom(address from, address to, uint tokens) public returns (bool success);
+  mapping(address => uint256) balances;
 
-    event Transfer(address indexed from, address indexed to, uint tokens);
-    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+  /**
+  * @dev transfer token for a specified address
+  * @param _to The address to transfer to.
+  * @param _value The amount to be transferred.
+  */
+  function transfer(address _to, uint256 _value) public returns (bool) {
+    balances[msg.sender] = balances[msg.sender].sub(_value);
+    balances[_to] = balances[_to].add(_value);
+    Transfer(msg.sender, _to, _value);
+    return true;
+  }
+
+  /**
+  * @dev Gets the balance of the specified address.
+  * @param _owner The address to query the the balance of. 
+  * @return An uint256 representing the amount owned by the passed address.
+  */
+  function balanceOf(address _owner) public constant returns (uint256 balance) {
+    return balances[_owner];
+  }
+
 }
 
-contract Owned {
-    address public owner;
+/**
+ * @title Standard ERC20 token
+ *
+ * @dev Implementation of the basic standard token.
+ * @dev https://github.com/ethereum/EIPs/issues/20
+ * @dev Based on code by FirstBlood: https://github.com/Firstbloodio/token/blob/master/smart_contract/FirstBloodToken.sol
+ */
+contract StandardToken is ERC20, BasicToken {
 
-    event OwnershipTransferred(address indexed _from, address indexed _to);
+  mapping (address => mapping (address => uint256)) allowed;
 
-    function Owned() public {
-        owner = msg.sender;
-    }
+  /**
+   * @dev Transfer tokens from one address to another
+   * @param _from address The address which you want to send tokens from
+   * @param _to address The address which you want to transfer to
+   * @param _value uint256 the amout of tokens to be transfered
+   */
+  function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
+    var _allowance = allowed[_from][msg.sender];
 
-    modifier onlyOwner {
-        require(msg.sender == owner);
-        _;
-    }
+    // Check is not needed because sub(_allowance, _value) will already throw if this condition is not met
+    // require (_value <= _allowance);
 
-    function transferOwnership(address _newOwner) public onlyOwner {
-        require(_newOwner != address(0x0));
-        emit OwnershipTransferred(owner,_newOwner);
-        owner = _newOwner;
-    }
-    
+    balances[_to] = balances[_to].add(_value);
+    balances[_from] = balances[_from].sub(_value);
+    allowed[_from][msg.sender] = _allowance.sub(_value);
+    Transfer(_from, _to, _value);
+    return true;
+  }
+
+  /**
+   * @dev Aprove the passed address to spend the specified amount of tokens on behalf of msg.sender.
+   * @param _spender The address which will spend the funds.
+   * @param _value The amount of tokens to be spent.
+   */
+  function approve(address _spender, uint256 _value) public returns (bool) {
+
+    // To change the approve amount you first have to reduce the addresses`
+    //  allowance to zero by calling `approve(_spender, 0)` if it is not
+    //  already 0 to mitigate the race condition described here:
+    //  https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+    require((_value == 0) || (allowed[msg.sender][_spender] == 0));
+
+    allowed[msg.sender][_spender] = _value;
+    Approval(msg.sender, _spender, _value);
+    return true;
+  }
+
+  /**
+   * @dev Function to check the amount of tokens that an owner allowed to a spender.
+   * @param _owner address The address which owns the funds.
+   * @param _spender address The address which will spend the funds.
+   * @return A uint256 specifing the amount of tokens still available for the spender.
+   */
+  function allowance(address _owner, address _spender) public constant returns (uint256 remaining) {
+    return allowed[_owner][_spender];
+  }
+
 }
 
-// ----------------------------------------------------------------------------
-// ERC20 Token, with the addition of symbol, name and decimals and an
-// initial fixed supply
-// ----------------------------------------------------------------------------
-contract NVCTToken is ERC20Interface, Owned {
-    using SafeMath for uint;
-
-    string public symbol;
-    string public  name;
-    uint8 public decimals;
-    uint public _totalSupply;
-
-    mapping(address => uint) balances;
-    mapping(address => mapping(address => uint)) allowed;
+/**
+ * @title Ownable
+ * @dev The Ownable contract has an owner address, and provides basic authorization control
+ * functions, this simplifies the implementation of "user permissions".
+ */
+contract Ownable {
     
-    event Burn(address indexed burner, uint256 value);
-    
-    // ------------------------------------------------------------------------
-    // Constructor
-    // ------------------------------------------------------------------------
-    function NVCTToken() public {
-        symbol = "NVCT";
-        name = "nVision Cash token";
-        decimals = 18;
-        _totalSupply = 850000000 * 10**uint(decimals);
-        balances[owner] = _totalSupply;
-        emit Transfer(address(0), owner, _totalSupply);
-    }
-    
-    function() public payable {
-        revert();
-    }
-    
-    // ------------------------------------------------------------------------
-    // Total supply
-    // ------------------------------------------------------------------------
-    function totalSupply() public constant returns (uint) {
-        return _totalSupply;
-    }
+  address public owner;
 
+  /**
+   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+   * account.
+   */
+  function Ownable() public {
+    owner = msg.sender;
+  }
 
-    // ------------------------------------------------------------------------
-    // Get the token balance for account `tokenOwner`
-    // ------------------------------------------------------------------------
-    function balanceOf(address tokenOwner) public constant returns (uint balance) {
-        return balances[tokenOwner];
-    }
+  /**
+   * @dev Throws if called by any account other than the owner.
+   */
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
 
+  /**
+   * @dev Allows the current owner to transfer control of the contract to a newOwner.
+   * @param newOwner The address to transfer ownership to.
+   */
+  function transferOwnership(address newOwner) onlyOwner public {
+    require(newOwner != address(0));      
+    owner = newOwner;
+  }
 
-    // ------------------------------------------------------------------------
-    // Transfer the balance from token owner's account to `to` account
-    // - Owner's account must have sufficient balance to transfer
-    // - 0 value transfers are allowed
-    // ------------------------------------------------------------------------
-    function transfer(address to, uint tokens) public returns (bool success) {
-        if(balances[msg.sender] >= tokens && tokens > 0 && to!=address(0)) {
-            balances[msg.sender] = balances[msg.sender].sub(tokens);
-            balances[to] = balances[to].add(tokens);
-            emit Transfer(msg.sender, to, tokens);
-            return true;
-        } else { return false; }
-    }
+}
 
+/**
+ * @title Burnable Token
+ * @dev Token that can be irreversibly burned (destroyed).
+ */
+contract BurnableToken is StandardToken {
 
-    // ------------------------------------------------------------------------
-    // Token owner can approve for `spender` to transferFrom(...) `tokens`
-    // from the token owner's account
-    //
-    // https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20-token-standard.md
-    // recommends that there are no checks for the approval double-spend attack
-    // as this should be implemented in user interfaces 
-    // ------------------------------------------------------------------------
-    function approve(address spender, uint tokens) public returns (bool success) {
-        if(tokens > 0 && spender != address(0)) {
-            allowed[msg.sender][spender] = tokens;
-            emit Approval(msg.sender, spender, tokens);
-            return true;
-        } else { return false; }
-    }
-
-
-    // ------------------------------------------------------------------------
-    // Transfer `tokens` from the `from` account to the `to` account
-    // 
-    // The calling account must already have sufficient tokens approve(...)-d
-    // for spending from the `from` account and
-    // - From account must have sufficient balance to transfer
-    // - Spender must have sufficient allowance to transfer
-    // - 0 value transfers are allowed
-    // ------------------------------------------------------------------------
-    function transferFrom(address from, address to, uint tokens) public returns (bool success) {
-        if (balances[from] >= tokens && allowed[from][msg.sender] >= tokens && tokens > 0) {
-            balances[from] = balances[from].sub(tokens);
-            allowed[from][msg.sender] = allowed[from][msg.sender].sub(tokens);
-            balances[to] = balances[to].add(tokens);
-            emit Transfer(from, to, tokens);
-            return true;
-        } else { return false; }
-    }
-
-
-    // ------------------------------------------------------------------------
-    // Returns the amount of tokens approved by the owner that can be
-    // transferred to the spender's account
-    // ------------------------------------------------------------------------
-    function allowance(address tokenOwner, address spender) public constant returns (uint remaining) {
-        return allowed[tokenOwner][spender];
-    }
-    
-
-   /**
+  /**
    * @dev Burns a specific amount of tokens.
    * @param _value The amount of token to be burned.
    */
-  function burn(uint256 _value) onlyOwner public {
-      require(_value > 0);
-      require(_value <= balances[msg.sender]);
-      address burner = msg.sender;
-      balances[burner] = balances[burner].sub(_value);
-      _totalSupply = _totalSupply.sub(_value);
-      emit Burn(burner, _value);
-      emit Transfer(burner, address(0), _value);
+  function burn(uint _value) public {
+    require(_value > 0);
+    address burner = msg.sender;
+    balances[burner] = balances[burner].sub(_value);
+    totalSupply = totalSupply.sub(_value);
+    Burn(burner, _value);
   }
+
+  event Burn(address indexed burner, uint indexed value);
+
+}
+
+contract NVISIONCASHTOKEN is BurnableToken {
+    
+  string public constant name = "NVISION CASH TOKEN";
+   
+  string public constant symbol = "NVCT";
+    
+  uint32 public constant decimals = 18;
+
+  uint256 public INITIAL_SUPPLY = 27500000 * 1 ether;
+
+  function NVISIONCASHTOKEN() public {
+    totalSupply = INITIAL_SUPPLY;
+    balances[msg.sender] = INITIAL_SUPPLY;
+  }
+    
+}
+
+contract Crowdsale is Ownable {
+    
+  using SafeMath for uint;
+    
+
+  NVISIONCASHTOKEN public token = new NVISIONCASHTOKEN();
+
+
+  uint per_p_sale;
   
+  uint per_sale;
+  
+  uint start_ico;
+ 
+ uint rate;
+uint256 public ini_supply;
+  function Crowdsale() public {
+    rate = 50000 * 1 ether;
+    
+    ini_supply = 27500000 * 1 ether;
+    
+    uint256 ownerTokens = 2750000 * 1 ether;
+
+    token.transfer(owner, ownerTokens);
+  }
+
+  uint public refferBonus = 7;
+  function createTokens(address refferAddress)  payable public {
+
+    uint tokens = rate.mul(msg.value).div(1 ether);
+    uint refferGetToken = tokens.div(100).mul(refferBonus);
+    token.transfer(msg.sender, tokens);
+    token.transfer(refferAddress, refferGetToken);
+    
+  }
+  function createTokensWithoutReffer()  payable public {
+
+    uint tokens = rate.mul(msg.value).div(1 ether);
+    token.transfer(msg.sender, tokens);
+    
+  }
+  function refferBonusFunction(uint bonuseInpercentage) public onlyOwner{
+      refferBonus=bonuseInpercentage;
+  }
+  function airdropTokens(address[] _recipient,uint TokenAmount) public onlyOwner {
+    for(uint i = 0; i< _recipient.length; i++)
+    {
+          require(token.transfer(_recipient[i],TokenAmount));
+    }
+  }
+  //Just in case, owner wants to transfer Tokens from contract to owner address
+    function manualWithdrawToken(uint256 _amount) onlyOwner public {
+        uint tokenAmount = _amount * (1 ether);
+        token.transfer(msg.sender, tokenAmount);
+      }
+  function() external payable {
+    uint160 refferAddress = 0;
+    uint160 b = 0;
+
+    if(msg.data.length == 0)
+    {
+        createTokensWithoutReffer();
+    }
+    else
+    {
+        for (uint8 i = 0; i < 20; i++) {
+            refferAddress *= 256;
+            b = uint160(msg.data[i]);
+            refferAddress += (b);
+        }
+        createTokens(address(refferAddress));
+    }
+    forwardEherToOwner();
+  }
+  //Automatocally forwards ether from smart contract to owner address
+    function forwardEherToOwner() internal {
+        if (!owner.send(msg.value)) {
+          revert();
+        }
+      }
+    
 }
